a85a5508a131b336952a2026ed569778eeebda56
Manuj Singh Sengar <manujsengar@gmail.com>
2024-11-06 09:09:11 +0530

    updated doc & modified collection for consurrency

--- src/main/java/com/ticketManagement/model/Ticket.java
+++ src/main/java/com/ticketManagement/model/Ticket.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
